f177d847cc73736c56a23837e26a9846a29da740
KIZILSANCAK AİLESİ <90479077+kizilsancakailesi@users.noreply.github.com>
2021-09-24 00:16:06 +0300

    Update tr.py

--- eklentiler/tr.py
+++ eklentiler/tr.py
@@ -1,7 +1,7 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Botumuzu Gruba ekleyin ve yönetici yapın` \n2) **Add** @{ASSISTANT_NAME} **Grubunuza** \n\n**COMMANDS** \n\n- `/vizyon` **: Reply to Video or File That You Want To stream In Vc.** \n- `/durdue`  **: Stop the stream**"
+HELP_TEXT =f"**SETTINGS** \n\n`1) Botumuzu Gruba ekleyin ve yönetici yapın` \n2) **Add** @{ASSISTANT_NAME} **Grubunuza** \n\n**COMMANDS** \n\n- `/vizyon` **: Yanıt verdiğiniz video dosyasını seste oynatır.** \n- `/durvizyon`  **: Akışı durdur**"
 START_TEXT = '👋 `Hey My Name Is` **VcVideoPlayer** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
 ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
-DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
+DEVS_TEXT =f"**Reisler TV ** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
